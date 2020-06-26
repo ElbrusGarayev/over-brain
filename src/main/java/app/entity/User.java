d9d309632d9d4ebf5a8dd8832f6590efe888d065
ElbrusGarayev <60807221+ElbrusGarayev@users.noreply.github.com>
2020-06-27 00:18:21 +0400

    User Entity created

--- src/main/java/app/entity/User.java
+++ src/main/java/app/entity/User.java
@@ -3,16 +3,36 @@ package app.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @AllArgsConstructor
 @Data
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull
+    private String fullname;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private byte[] photo;
+
+    @NotNull
+    private String lastseen;
+
+    @NotNull
+    private String Status;
 }
