b3e8be319d5143ec200be21514a8b4af5c830426
ElbrusGarayev <60807221+ElbrusGarayev@users.noreply.github.com>
2020-06-26 23:17:26 +0400

    Just addition

--- src/main/java/app/entity/User.java
+++ src/main/java/app/entity/User.java
@@ -1,4 +1,18 @@
 package app.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 }
--- src/main/java/app/repository/UserRepo.java
+++ src/main/java/app/repository/UserRepo.java
@@ -1,4 +1,7 @@
 package app.repository;
 
-public class UserRepo {
+import app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
 }
